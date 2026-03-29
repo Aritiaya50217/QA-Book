feebf8b4ca36fdce0350d93df69db60d73d9570f
artitaya <artitaya2466@gmail.com>
2026-03-29 23:17:05 +0700

    hooks

---
+++ chapter01quick-setup-refresher/06understanding-playwright-is-test-runner-basics/01using-parallel-execution-and-test-isolation/hooks.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.getByPlaceholder("Username").fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page).toHaveURL(/inventory.html/);
+});
+
+test('click inventory page title', async ({ page }) => {
+    await expect(page).toHaveTitle('Swag Labs');
+});
+
+test('Check shopping cart link is visible', async ({ page }) => {
+    const cartLink = page.locator('.shopping_cart_container');
+    await expect(cartLink).toBeVisible();
+});
+
+test.afterEach(async ({ page }) => {
+    await page.close(); // ปิด tab หลังแต่ละ test
+});
